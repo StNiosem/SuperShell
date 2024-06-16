3474ebdd1ddaa18b06376e16fc189a884bfc5837
StNiosem <bauch.aristide@gmail.com>
2024-06-16 21:55:03 +0200

    added main logic and Super for shit code and Shell for actual code

---
+++ main.c
@@ -0,0 +1,17 @@
+// SuperShell
+// A Shell for the future
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+// Custom Code
+#include "includes/super.h"
+#include "includes/shell.h"
+
+// Variables
+#define screenRes "screenkey.self.screenRes"
+
+void main() {
+
+}
