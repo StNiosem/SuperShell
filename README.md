9161b8cb57518ce3199e81edbd4209915b592391
StNiosem <bauch.aristide@gmail.com>
2024-06-18 18:38:06 +0200

    Added KeyboardEventListener function and updated >README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SuperShell
 SuperShell is the new shell for PnOS
+
+# Installation
+## Windows 
+* lol no
+## Linux
+* `gcc main.c -o supersh`
--- includes/super.h
+++ includes/super.h
@@ -6,11 +6,16 @@
 #define HEADER_SELF "super.h"
 #define SHELLENGINE "superEngine"
 
+//Version strings
 #define VERSION "1.0.0"
 #define SUPER_FRAMEWORK_VERSION "0.10-build*0fc4b4"
-
+#define INPUT_HANDLER_VERSION "1.0.0.01-build*e35b8a"
 
 void shellInit() {
     printf("Launching SuperShell\n\n");
     printf("SuperSH version v%s (shell framework v%s , Super Framework v%s)\n",VERSION, SHELL_FRAMEWORK_VERSION, SUPER_FRAMEWORK_VERSION);
+}
+
+void shellInputHandler() {
+	printf("Input Handler v%c", INPUT_HANDLER_VERSION);
 }
